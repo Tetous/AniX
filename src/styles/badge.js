1cf7ac24e03ab0438d6eba465b11900c898ac89f
manikandanraji <manikandan48711113@gmail.com>
2021-01-10 16:58:50 +0530

    major rewrite

--- src/styles/badge.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export default styled.span`
-  background-color: ${(props) => props.theme.blue};
-  color: ${(props) => props.theme.white};
-  padding: 0.6rem 0.8rem;
-  border-radius: 4px;
-`;
